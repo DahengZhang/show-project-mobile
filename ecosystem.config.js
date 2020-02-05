c903648c1f824c1aa330b7fe40338bdfdeedb618
DahengZhang <Daheng_Zhang@outlook.com>
2020-02-05 15:55:14 +0800

    启动命令

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'show-project-mobile',
-        script: 'start',
+        script: 'npm run start',
         instances: 1,
         autorestart: true,
         watch: false,
