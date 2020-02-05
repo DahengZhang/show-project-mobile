a3fa5e9807e7dd012e024ebc81ece515b5dbb235
DahengZhang <Daheng_Zhang@outlook.com>
2020-02-05 15:36:35 +0800

    运行前打包

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'show-project-mobile',
-        script: './app.js',
+        script: 'start',
         instances: 1,
         autorestart: true,
         watch: false,
